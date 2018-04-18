cbf0dbb70834a768ca1ed19ee871fc0408ea2b1c
bazarum <juantomas.garcia@gmail.com>
2018-04-18 17:56:04 +0200

    small and simple (and dumb) agent to test the gym_abadia

---
+++ agentv1.py
@@ -0,0 +1,15 @@
+import gym
+import gym_abadia
+
+env = gym.make('Abadia-v0')
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render(mode="human")
+        print("observation: {}\n", observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+
