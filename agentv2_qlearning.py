47477d12c9a8755771204612da4a218ad50e5d4c
bazarum <juantomas.garcia@gmail.com>
2018-04-22 15:49:19 +0200

    new ascii grid to show visited cells

--- agentv2_qlearning.py
+++ agentv2_qlearning.py
@@ -5,6 +5,25 @@ import gym
 import gym_abadia
 import numpy as np
 
+def pintaRejilla():
+    print("")
+    print("+---------------------+")
+    for xx in range(x-10, x+10):
+        print("|", end="")
+        for yy in range(y-10, y+10):
+            if (xx == x and yy == y):
+                print("G", end="")
+            else:
+                if (xx == adsoX and yy == adsoY):
+                    print("A", end="")
+                else:
+                    if (Visited[xx,yy] == 0):
+                        print(" ", end="")
+                    else:
+                        print("v", end="")
+        print("|")
+    print("+---------------------+")
+
 env = gym.make('Abadia-v0')
 
 #Initialize table with all zeros
@@ -28,13 +47,17 @@ for i_episode in range(50):
     for t in range(5000):
         x = int(state['Guillermo']['posX'])
         y = int(state['Guillermo']['posY'])
+
+        adsoX = int(state['Adso']['posX'])
+        adsoY = int(state['Adso']['posY'])
+
         ori = int(state['Guillermo']['orientacion'])
         # env.render(mode="human")
 
         # Choose an action by greedily (with noise) picking from Q table
-        offset = np.random.randn(1, env.action_space.n) * (1. / (t + 1))
-        action = np.argmax(Q[x, y, :] + offset)
-        print ("offset {}".format(offset))
+        noise = np.random.randn(1, env.action_space.n) * (1. / (t + 1))
+        action = np.argmax(Q[x, y, :] + noise)
+        # print ("noise {}".format(noise))
         # Get new state and reward from environment
         newState, reward, done, info = env.step(action)
 
@@ -46,17 +69,22 @@ for i_episode in range(50):
         newX = int(newState['Guillermo']['posX'])
         newY = int(newState['Guillermo']['posY'])
 
+
         if (Visited[newX, newY] == 0):
             reward += 0.1
         if (Visited[newX, newY] >=10):
             reward -= 0.1
-        Visited[newX, newY] += 1
 
+        Visited[newX, newY] += 1
 
         # Update Q-Table with new knowledge
         Q[x, y, action] = Q[x, y, action] + lr * (reward + yy * np.max(Q[newX, newY, :]) - Q[x, y, action])
         print ("Episode:{}:{} X,Y->{},{},{} A({}) r:{} tr:{} Q(s,a)= {}".format(i_episode, t, x, y,
-                                                                     ori, action, reward, rAll, Q[x,y]), end="\n")
+                                                                     ori, action, reward, rAll, Q[x,y]), end="\r")
+
+        if (t % 20 == 0):
+            pintaRejilla()
+
         rAll += reward
         state = newState
         if done == True:
@@ -69,3 +97,4 @@ print("Score over time: " + str(sum(rList)/100))
 
 print("Final Q-Table Values")
 print(Q)
+
