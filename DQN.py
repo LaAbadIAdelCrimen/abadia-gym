ab69ede6650722a5b105b3ca7063ee9e67d666e9
bazarum <juantomas.garcia@gmail.com>
2018-05-21 23:04:53 +0200

    DQN.py will live at AbadIA git module

---
+++ DQN.py
@@ -0,0 +1,79 @@
+import random
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+
+from collections import deque
+
+class DQN:
+    def __init__(self, env):
+        self.env     = env
+        self.memory  = deque(maxlen=2000)
+
+        self.gamma = 0.85
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.005
+        self.tau = .125
+
+        self.model        = self.create_model()
+        self.target_model = self.create_model()
+
+    def create_model(self):
+        model   = Sequential()
+        state_shape  = self.env.observation_space.shape
+        model.add(Dense(24, input_dim=state_shape[0], activation="relu"))
+        model.add(Dense(48, activation="relu"))
+        model.add(Dense(24, activation="relu"))
+        model.add(Dense(self.env.action_space.n))
+        model.compile(loss="mean_squared_error",
+            optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def act(self, state):
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+
+        vector = self.env.stateVector()
+        self.env.vector = vector
+
+        if np.random.random() < self.epsilon:
+            action = self.env.action_space.sample()
+            # print("e-greedy: {}".format(action))
+        else:
+            predictions = self.model.predict(vector)[0]
+            action = np.argmax(predictions)
+            # print("vector: {} predictions: {} action: {}".format(vector, predictions, action))
+
+        return action
+
+    def remember(self, state, action, reward, new_state, done):
+        self.memory.append([state, action, reward, new_state, done])
+
+    def replay(self):
+        batch_size = 32
+        if len(self.memory) < batch_size:
+            return
+
+        samples = random.sample(self.memory, batch_size)
+        for sample in samples:
+            state, action, reward, new_state, done = sample
+            target = self.target_model.predict(state)
+            if done:
+                target[0][action] = reward
+            else:
+                Q_future = max(self.target_model.predict(new_state)[0])
+                target[0][action] = reward + Q_future * self.gamma
+            self.model.fit(state, target, epochs=1, verbose=0)
+
+    def target_train(self):
+        weights = self.model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(target_weights)):
+            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
+        self.target_model.set_weights(target_weights)
+
+    def save_model(self, fn):
+        self.model.save(fn)
