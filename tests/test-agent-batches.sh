52603cc87963e1760b608f8765ad739bf806a9ea
bazarum <juantomas.garcia@gmail.com>
2018-09-09 22:33:10 +0200

    add beta for pubsub commands

--- tests/test-agent-batches.sh
+++ tests/test-agent-batches.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # gcloud init
-gcloud pubsub topics create agent-batches
-gcloud pubsub subscriptions create --topic agent-batches agent-batch
-gcloud pubsub topics publish myTopic --message "--episodes=1 --steps=50 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80"
-gcloud pubsub subscriptions pull --auto-ack agent-batch
+gcloud beta pubsub topics create agent-batches
+gcloud beta pubsub subscriptions create --topic agent-batches agent-batch
+gcloud beta pubsub topics publish myTopic --message "--episodes=1 --steps=50 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80"
+gcloud beta pubsub subscriptions pull --auto-ack agent-batch
