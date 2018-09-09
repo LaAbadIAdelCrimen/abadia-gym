8f5896306ccce0e3917e7fa499a8f1a2d1e72d25
bazarum <juantomas.garcia@gmail.com>
2018-09-09 23:34:48 +0200

    adding options to exit the loop when there are not more batches

--- loopbatches.sh
+++ loopbatches.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 while true
 do
-  gcloud beta pubsub subscriptions pull agent-batch
-  # python3 agentv4_dqn.py --episodes=500 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 # -c /tmp/check # -c partidas/20180602/abadia_checkpoint_180602_214255_980367_2_1_4_21_25_0.checkpoint
+  options=`gcloud beta pubsub subscriptions pull agent-batch --limit=1 --format="value(DATA)"`
+  echo "I will execute $options"
+  case $options in
+    "exit")
+      echo "OK I will exit now"
+      exit 0
+      ;;
+    "")
+      echo "No more batches, no more fun ..."
+      exit 0
+      ;;
+    *)
+      # python3 agentv4_dqn.py --episodes=500 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 # -c /tmp/check # -c partidas/20180602/abadia_checkpoint_180602_214255_980367_2_1_4_21_25_0.checkpoint
+      ;;
+  esac
+  sleep 1
 done
