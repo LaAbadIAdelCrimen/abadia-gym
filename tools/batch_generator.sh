f26b496ecb514bc2e9012ad51c8a245e2ac36652
bazarum <juantomas.garcia@gmail.com>
2018-09-09 23:44:25 +0200

    refactoring the loopbatches and the first batches generator

--- tools/batch_generator.sh
+++ tools/batch_generator.sh
@@ -6,6 +6,8 @@
 (find games -name "*.checkpoint" | sort -r | grep $1 | head -500) |
 while read -r line
 do
-  gcloud beta pubsub topics publish agent-batches "--episodes=1 --steps=200 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 -c $line "
+  options="--episodes=1 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 -c $line "
+  echo "Queuing -> ($options) into the topic agent-batches"
+  gcloud beta pubsub topics publish agent-batches "$options"
 done
 
