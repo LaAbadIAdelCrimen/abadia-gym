e3984e941efd7d5ecc40ab245e084878778f7eaf
bazarum <juantomas.garcia@gmail.com>
2018-09-09 22:32:08 +0200

    commenting the gcloud init option

--- tests/test-agent-batches.sh
+++ tests/test-agent-batches.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcloud init
+# gcloud init
 gcloud pubsub topics create agent-batches
 gcloud pubsub subscriptions create --topic agent-batches agent-batch
 gcloud pubsub topics publish myTopic --message "--episodes=1 --steps=50 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80"
