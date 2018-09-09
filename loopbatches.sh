125e64a464beb9a048b0217d56f0c72dcd100282
bazarum <juantomas.garcia@gmail.com>
2018-09-09 23:49:33 +0200

    fixed a minor bug

--- loopbatches.sh
+++ loopbatches.sh
@@ -13,7 +13,8 @@ do
       exit 0
       ;;
     *)
-      # python3 agentv4_dqn.py --episodes=500 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 # -c /tmp/check # -c partidas/20180602/abadia_checkpoint_180602_214255_980367_2_1_4_21_25_0.checkpoint
+      python3 agentv4_dqn.py $options
+      # --episodes=500 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 # -c /tmp/check # -c partidas/20180602/abadia_checkpoint_180602_214255_980367_2_1_4_21_25_0.checkpoint
       ;;
   esac
   sleep 1
