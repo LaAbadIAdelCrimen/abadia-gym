d32a495eb6312eca9544430a67122d51db845806
bazarum <juantomas.garcia@gmail.com>
2018-09-11 08:34:29 +0200

    deleting the checkpoints file with 0 obsequium

--- tools/batch_generator.sh
+++ tools/batch_generator.sh
@@ -3,10 +3,10 @@
 # use the $1 to grep some pattern from the last checkpoints that was autosaved
 # example: _21_ would select the room 21 or not ;-P
 
-(find games -name "*.checkpoint" | sort -r | grep $1 | head -500) |
+(find games -name "*.checkpoint" | sort -r | grep $1 | grep -v "*_0_0.*" | head -500) |
 while read -r line
 do
-  options="--episodes=5 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 -c $line "
+  options="--episodes=4 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 -c $line "
   echo "Queuing -> ($options) into the topic agent-batches"
   gcloud beta pubsub topics publish agent-batches "$options"
 done
