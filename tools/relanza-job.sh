2bb9afc5171496299e25f28900a1b8b982337283
bazarum <juantomas.garcia@gmail.com>
2018-09-13 09:36:00 +0200

    also try to tail the log of the agent

--- tools/relanza-job.sh
+++ tools/relanza-job.sh
@@ -4,3 +4,5 @@ kubectl delete job abadia-pubsub
 kubectl create -f k8s/job-pubsub.yaml
 kubectl get jobs
 kubectl describe jobs/abadia-pubsub
+job=`kubectl describe jobs/abadia-pubsub | grep Created | cut -d":" -f2`
+kubectl logs -f $job agent
