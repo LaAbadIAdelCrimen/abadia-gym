b865d5e787f6726f7a6c68ddeacee4770886f357
bazarum <juantomas.garcia@gmail.com>
2018-09-21 17:47:24 +0200

    the list of all games is created at the bucket

--- tools/create_all_games_list.sh
+++ tools/create_all_games_list.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gsutil ls -r "gs://abadia-data/games/**"
+gsutil ls -r "gs://abadia-data/games/**" | grep "abadia_game" > /tmp/s1
+gsutil cp /tmp/s1 gs://abadia-data/all_game_list.txt
+
