b0e7531aad20746854df974ef0931ea57d555311
bazarum <juantomas.garcia@gmail.com>
2018-09-21 14:35:27 +0200

    New batch remote generator

---
+++ tools/batch_generator_remote.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# use the $1 to grep some pattern from the last checkpoints that was autosaved
+# example: _21_ would select the room 21 or not ;-P
+
+(gsutil ls -r "gs://abadia-data/games/**" | grep checkpoint | sed -e 's/gs:\/\/abadia-data\///g'| grep $1 | grep -v "*_0_0.*" | head -500) |
+while read -r line
+do
+  case $line in
+    *_0_0.c*)
+      echo "look like a dead checkpoint, I don't queue it"
+      ;;
+    *)
+      options="--episodes=3 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://localhost -p 4477 -c $line "
+      echo "Queuing -> ($options) into the topic agent-batches"
+      gcloud beta pubsub topics publish agent-batches "$options"
+      ;;
+  esac
+done
+
