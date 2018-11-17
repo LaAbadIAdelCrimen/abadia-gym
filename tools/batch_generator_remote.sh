cbb1955fd49ef29d8ff93edd17cdfd92d7a57271
bazarum <juantomas.garcia@gmail.com>
2018-11-17 16:23:14 +0100

    updating the new options for queuing messages

--- tools/batch_generator_remote.sh
+++ tools/batch_generator_remote.sh
@@ -3,17 +3,17 @@
 # use the $1 to grep some pattern from the last checkpoints that was autosaved
 # example: _21_ would select the room 21 or not ;-P
 
-(gsutil ls -r "gs://abadia-data/games/**" | grep checkpoint | sed -e 's/gs:\/\/abadia-data\///g'| grep $1 | grep -v "*_0_0.*" | head -500) |
+(gsutil ls -r "gs://abadia-data/games/**" | grep checkpoint | sed -e 's/gs:\/\/abadia-data\///g'| grep $1 | grep -v "_0_0.che" | head -500) |
 while read -r line
 do
   case $line in
     *_0_0.c*)
-      echo "look like a dead checkpoint, I don't queue it"
+      echo "look like a dead checkpoint, I don't queue it ($line)"
       ;;
     *)
-      options="--episodes=3 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://localhost -p 4477 -c $line "
+      options="--episodes=10 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://localhost -p 4477 -c $line "
       echo "Queuing -> ($options) into the topic agent-batches"
-      gcloud beta pubsub topics publish agent-batches "$options"
+      gcloud beta pubsub topics publish agent-batches --message "$options"
       ;;
   esac
 done
