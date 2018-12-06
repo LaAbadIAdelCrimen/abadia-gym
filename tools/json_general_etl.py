32635f63698274b0d87d80f8bccb49176b8eff08
bazarum <juantomas.garcia@gmail.com>
2018-12-06 16:05:21 +0100

    making some clean ups with the jsons

---
+++ tools/json_general_etl.py
@@ -0,0 +1,33 @@
+import os
+import json
+
+def load_game(name):
+
+    print(name);
+    with open(name) as json_data:
+        d = json.load(json_data)
+        print(d)
+
+
+folders = []
+files = []
+current_path = ""
+
+for entry in os.scandir('games'):
+    if entry.is_dir():
+        folders.append(entry.path)
+        current_path = entry.path
+        for fi in os.scandir(entry.path):
+            if fi.is_file():
+                files.append(fi.path)
+    elif entry.is_file():
+        files.append(entry.path)
+
+print('Files:')
+print(files)
+for file in files:
+    if file.endswith("point") == False:
+        load_game(file)
+
+"pp.json".endswith("json")
+
