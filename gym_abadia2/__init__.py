d30ec77658ff81ca8a63a23d1fc6a31dae2df2f0
bazarum <juantomas.garcia@gmail.com>
2019-01-14 20:41:37 +0100

    creating the entry point for the new gym

---
+++ gym_abadia2/__init__.py
@@ -0,0 +1,9 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='Abadia-v2',
+    entry_point='gym_abadia.envs:AbadiaEnv2',
+)
