1b810114570eec4d64a603c56a1eed8ebd3133c1
bazarum <juantomas.garcia@gmail.com>
2019-01-16 15:24:36 +0100

    Creating a new env

---
+++ gym_abadia/gym_abadia/envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_abadia.envs.abadia_env import AbadiaEnv
+from gym_abadia.envs.abadia_env import AbadiaEnv2
---
+++ gym_abadia/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='gym_abadia',
+      version='0.0.3',
+      install_requires=['gym>=0.2.3', 'pandas']
+      )
+
