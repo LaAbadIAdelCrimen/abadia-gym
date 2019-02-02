3c25f5f2712e6e7e4eb43f50a96eb3a873d9e2eb
bazarum <juantomas.garcia@gmail.com>
2019-02-02 23:31:45 +0100

    reading action files to create training dataset for value functions

---
+++ tools/actions_json_etl_value.py
@@ -0,0 +1,40 @@
+import os
+import json
+import numpy as np
+
+
+def load_actions(name):
+    print("-----------------------")
+    print(name);
+    with open(name) as json_data:
+        lines = json_data.readlines()
+        if lines:
+            for line in lines:
+                # print("line:"+line)
+                if (len(line) > 0 and line.startswith("[")):
+                    d = json.loads(line)
+                    # print("json{}".format(d))
+                    print("Rejilla: {}".format(d[0]['action']['nextstate']['Rejilla']))
+    print("-----------------------")
+
+folders = []
+files = []
+current_path = ""
+
+for entry in os.scandir('games'):
+    if entry.is_dir():
+        folders.append(entry.path)
+        current_path = entry.path
+        for fi in os.scandir(entry.path):
+            if fi.is_file():
+                files.append(fi.path)
+    elif entry.is_file():
+        files.append(entry.path)
+
+print('Files:')
+print(files)
+for file in files:
+    if  "abadia_actions" in file and "20190202" in file:
+        load_actions(file)
+
+
