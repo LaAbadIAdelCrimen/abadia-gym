df0596b3bcfd797b6e9173fdfa025c1f75c5405f
bazarum <juantomas.garcia@gmail.com>
2019-03-26 19:08:54 +0100

    mini tools for create json datasets using a simple regular expresion

--- tools/create_datasets.sh
+++ tools/create_datasets.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 echo "Creating $2  list with this query ($1)"
-gsutil ls -r "gs://abadia-data/games/**" | fgrep "$1" | sort -rn | sed  "s/gs:\/\//https:\/\/storage.googleapis.com\//"> /tmp/s1
+gsutil ls -r "gs://abadia-data/games/**" | grep -E "$1" | sort -rn | sed  "s/gs:\/\//https:\/\/storage.googleapis.com\//"> /tmp/s1
 gsutil cp /tmp/s1 gs://abadia-data/datasets/$2.txt
 echo "Done: list created"
-echo "Getting all the files to build the $2.tgz"
+echo "Getting all the files to build the $2.tar"
 
-rm -f /tmp/data
+rm -rf /tmp/data
 mkdir -p /tmp/data
 cd /tmp/data
-rm -f $2.tgz
 
 cat /tmp/s1 |
 while read -r line
@@ -18,9 +17,11 @@ do
   wget $line  2>/dev/null
 done
 echo "DONE: now we will pack in a tgz file"
-tar cvzf $2.tgz *
-ls -lt *.tgz
+tar cvzf /tmp/$2.tgz *
+ls -lt /tmp/*.tar
 ls -lt *.json
 echo "COPYING is to GCP"
-gsutil cp $2.tgz gs://abadia-data/datasets/$2.tgz
-gsutils ls -l gs://abadia-data/datasets/$2.tgz
+gsutil cp /tmp/$2.tgz gs://abadia-data/datasets/$2.tgz
+gsutil ls -l gs://abadia-data/datasets/$2.tgz
+rm /tmp/$2.tgz
+
