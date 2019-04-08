44f6652c818384e806b1943c20a10561ec7e3326
bazarum <juantomas.garcia@gmail.com>
2019-04-07 20:17:47 -0700

    adding a little program to test the new NGDQN

---
+++ test_NGDQN.py
@@ -0,0 +1,10 @@
+import gym
+import gym_abadia
+import numpy as np
+import os
+import argparse
+import random
+import AbadIA.NGDQN
+AbadIA.NGDQN
+# env = gym.make('Abadia-v2')
+dqn_agent = AbadIA.NGDQN.NGDQN(env=None)
