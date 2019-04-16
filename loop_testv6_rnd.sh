07fd95b6eb4bcc7daccc48921d15accbab28fa09
bazarum <juantomas.garcia@gmail.com>
2019-04-16 12:50:50 +0200

    new loop to check the new v6 agent

--- loop_testv6_rnd.sh
+++ loop_testv6_rnd.sh
@@ -6,16 +6,18 @@
 while true
 do
 (find games -name "*.checkpoint" | grep 2019 | grep "$1" | grep  -v "_0_" | sort -r | head -50) |
-while read -r line
+while read -r file
 do
 
   echo "---------------------"
-  echo $line
+  echo $file
   echo "---------------------"
-  ls -lt $line
+  ls -lt $file
   sleep 1
   # python3 agenttestv2.py --episodes=1 --steps=9000 -c $line -g abadia-data  -p 4477
-  python3 agentv6_ngdqn.py --episodes=1 --steps=500 -g abadia-data --model=models/model_v6_lastest.model -g abadia-data -c $line
+  python3 agentv6_ngdqn.py --episodes=1 --steps=3000 -g abadia-data \
+  --inimodel=models/model_v6_lastest.model \
+  -g abadia-data -c $file
   # -s http://35.241.222.173 -p 80 # -c /tmp/check
 
 done
