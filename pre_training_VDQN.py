7bc1a08cefae9b3248858f7e227d095593bd2458
bazarum <juantomas.garcia@gmail.com>
2019-04-18 18:14:04 +0200

    pre training script for creating the value vectors for the VDQN

---
+++ pre_training_VDQN.py
@@ -0,0 +1,23 @@
+import gym
+import gym_abadia
+import numpy as np
+import os
+import argparse
+import random
+import json
+import AbadIA.NGDQN
+AbadIA.NGDQN
+
+print("Creating the Model v6 from the last version")
+dqn_agent = AbadIA.NGDQN.NGDQN(env=None, initModelName="models/last_v6.model", modelName="models/last_version_v6.model")
+
+print("Transforming some actions to vectors and saving it into a dir")
+dqn_agent.load_actions_from_a_dir_and_save_to_vectors("./games/20190418")
+
+print("Loading the vectors from a dir")
+# dqn_agent.load_vectors_from_a_dir("./games/20190416")
+
+
+
+
+
