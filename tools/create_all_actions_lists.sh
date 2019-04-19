5c5bf4152542a1b691d52b978c4c46f45771df5d
bazarum <juantomas.garcia@gmail.com>
2019-04-19 17:01:04 +0200

    optimizing creall all lists of files

--- tools/create_all_actions_lists.sh
+++ tools/create_all_actions_lists.sh
@@ -4,15 +4,15 @@ echo "Creating the mega abadia-data list"
 gsutil ls -r "gs://abadia-data/games/**" | sort -rn | sed  "s/gs:\/\//https:\/\/storage.googleapis.com\//"> /tmp/all
 gsutil cp /tmp/all gs://abadia-data/all_abadia_data_info.txt
 
-for group in "abadia_actions" "abadia_games" "abadia_values" ".checkpoint"
+for group in "abadia_actions" "abadia_game" "abadia_values" ".checkpoint"
 do
     echo "Creating all $group list gs://abadia-data/all_${group}_list.txt"
-    grep "$group" /tmp/all | gsutil cp -I gs://abadia-data/all_${group}_list.txt
+    grep $group /tmp/all | gsutil cp - gs://abadia-data/all_${group}_list.txt
 
     echo "Creating Last 5000 ${group} list"
-    grep "$group" /tmp/all | head -5000 | gsutil cp -I gs://abadia-data/last_5000_${group}_list.txt
+    grep $group /tmp/all | head -5000 | gsutil cp - gs://abadia-data/last_5000_${group}_list.txt
 
     echo "Creating Last 1000 ${group} list"
-    grep "$group" /tmp/all | head -1000 | gsutil cp -I gs://abadia-data/last_1000_${group}_list.txt
+    grep $group /tmp/all | head -1000 | gsutil cp - gs://abadia-data/last_1000_${group}_list.txt
 done
 
