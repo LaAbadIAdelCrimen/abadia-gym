38b9d2dd3a9a49c8cf434e2352fcb321b137d471
bazarum <juantomas.garcia@gmail.com>
2019-04-23 22:00:12 +0200

    fixing a minor error

--- tools/compress.sh
+++ tools/compress.sh
@@ -7,5 +7,14 @@ do
   echo "$file"
   gs=`echo $file | sed -e 's/https\:\/\/storage\.googleapis\.com/gs\:\//'`
   echo "$gs"
-  curl $file | gzip - | gsutil cp - $gs.gz && gsutil rm $gs
+  gsutil ls -l $file
+  ret=$?
+  case $ret in
+    0)
+        curl $file | gzip - | gsutil cp - $gs.gz && gsutil rm $gs
+        ;;
+    *)
+        echo "Looks like there is an problem with $file
+        ;;
+  esac
 done
