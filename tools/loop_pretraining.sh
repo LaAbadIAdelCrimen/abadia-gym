418a25d477c3524fe54fd0f93e2faca6da3d5bc0
bazarum <juantomas.garcia@gmail.com>
2019-04-23 22:36:16 +0200

    checking is the action file exist, before try to pre-training it

--- tools/loop_pretraining.sh
+++ tools/loop_pretraining.sh
@@ -20,7 +20,16 @@ do
       ;;
     *)
       echo "pretraining ${actions}"
-      python3 extract_vectors_from_actions.py $actions
+      gsutil ls -l $actions
+      ret=$?
+      case $ret in
+          0)
+            python3 extract_vectors_from_actions.py $actions
+            ;;
+          *)
+            echo "Looks like there is a problem with $actions"
+            ;;
+      esac
 
       # python3 $options
       # --episodes=500 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 # -c /tmp/check # -c partidas/20180602/abadia_checkpoint_180602_214255_980367_2_1_4_21_25_0.checkpoint
