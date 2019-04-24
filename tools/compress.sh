bf88862d915ea8af03d2b211713c5d1332502dff
bazarum <juantomas.garcia@gmail.com>
2019-04-24 10:16:01 +0200

    adding a k8s job to compress all old files

--- tools/compress.sh
+++ tools/compress.sh
@@ -2,7 +2,7 @@
 echo "updating the all everithing lists to speed it up"
 echo "and avoid to destroy existing gzips (we need to fix and improve it)"
 tools/create_all_actions_lists.sh
-for file in `curl https://storage.googleapis.com/abadia-data/all_abadia_actions_list.txt | grep _actions_190422 | grep -v ".gz"`
+for file in `curl https://storage.googleapis.com/abadia-data/all_abadia_actions_list.txt | grep $1 | grep -v ".gz"`
 do
   echo "$file"
   gs=`echo $file | sed -e 's/https\:\/\/storage\.googleapis\.com/gs\:\//'`
