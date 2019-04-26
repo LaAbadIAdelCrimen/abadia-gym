764cdda0863246b1fa95d3925c38bf14e5cb90e3
bazarum <juantomas.garcia@gmail.com>
2019-04-26 23:07:33 +0200

    new script for training policy vectors from pub/sub

--- tools/loop_vectors.sh
+++ tools/loop_vectors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-  options=`gcloud beta pubsub subscriptions pull training --auto-ack --limit=3 --format="value(DATA)"`
+  options=`gcloud beta pubsub subscriptions pull training --auto-ack --limit=23 --format="value(DATA)"`
   echo "I will pick a few vectors ---> $options"
   case $options in
     "exit")
@@ -30,6 +30,8 @@ do
       python3 ./training_NGDQN.py /tmp/vectors
       echo "cleaning the temporary dir"
       sleep 5
+      echo "uploading the updated model"
+      gsutil cp models/pre_last_model_v6.model gs://abadia-data/models/last_model_v6.model
       # --episodes=500 --steps=2000 --model=models/model_v1_lastest.model -g abadia-data -s http://35.241.222.173 -p 80 # -c /tmp/check # -c partidas/20180602/abadia_checkpoint_180602_214255_980367_2_1_4_21_25_0.checkpoint
       ;;
   esac
