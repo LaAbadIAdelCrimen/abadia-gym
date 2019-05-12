63eb11e0a092e44231228606dd72d36ef04e5720
bazarum <juantomas.garcia@gmail.com>
2019-05-12 15:27:19 +0200

    incremeting the size of the window when creating the files with all actions/check/vectors

--- tools/create_all_actions_lists.sh
+++ tools/create_all_actions_lists.sh
@@ -9,10 +9,10 @@ do
     echo "Creating all $group list gs://abadia-data/all_${group}_list.txt"
     grep $group /tmp/all | gsutil cp - gs://abadia-data/all_${group}_list.txt
 
-    echo "Creating Last 5000 ${group} list"
-    grep $group /tmp/all | head -5000 | gsutil cp - gs://abadia-data/last_5000_${group}_list.txt
+    echo "Creating Last 20000 ${group} list"
+    grep $group /tmp/all | head -5000 | gsutil cp - gs://abadia-data/last_20000${group}_list.txt
 
-    echo "Creating Last 1000 ${group} list"
-    grep $group /tmp/all | head -1000 | gsutil cp - gs://abadia-data/last_1000_${group}_list.txt
+    echo "Creating Last 5000 ${group} list"
+    grep $group /tmp/all | head -1000 | gsutil cp - gs://abadia-data/last_5000_${group}_list.txt
 done
 
