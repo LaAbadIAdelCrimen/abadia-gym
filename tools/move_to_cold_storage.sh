7c8eb82d1c8524deb3cbde6ab18658c0e3a8e81d
bazarum <juantomas.garcia@gmail.com>
2019-05-15 10:39:11 +0200

    move to cold storage is ready

--- tools/move_to_cold_storage.sh
+++ tools/move_to_cold_storage.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Moving the $1 directory to the AbadIA Data cold storage"
+gsutil ls "gs://abadia-data/games/$1" | while read -r line
+do
+  echo $line
+  file=`echo $line | sed -e 's/gs\:\/\/abadia-data\///g'`
+  gsutil cp $line gs://abadia-data-cold/$file
+done
