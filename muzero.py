7a2c41b443afa9bada8f8de4f70efa15068b38b9
bazarum <juantomas.garcia@gmail.com>
2021-01-08 22:22:50 +0100

    new gym right use for muzero

---
+++ muzero.py
@@ -0,0 +1,38 @@
+from muzero.config import MuZeroConfig, make_abadia_config
+from muzero.networks.shared_storage import SharedStorage
+from muzero.self_play.self_play import run_selfplay, run_eval
+from muzero.training.replay_buffer import ReplayBuffer
+from muzero.training.training import train_network
+import gym
+import gym_abadia.envs
+
+def muzero(config: MuZeroConfig):
+    """
+    MuZero training is split into two independent parts: Network training and
+    self-play data generation.
+    These two parts only communicate by transferring the latest networks checkpoint
+    from the training to the self-play, and the finished games from the self-play
+    to the training.
+    In contrast to the original MuZero algorithm this version doesn't works with
+    multiple threads, therefore the training and self-play is done alternately.
+    """
+    config.new_network()
+    storage = SharedStorage(config.new_network(), config.uniform_network(), config.new_optimizer())
+    replay_buffer = ReplayBuffer(config)
+
+    for loop in range(config.nb_training_loop):
+        print("Training loop", loop)
+        score_train = run_selfplay(config, storage, replay_buffer, config.nb_episodes)
+        train_network(config, storage, replay_buffer, config.nb_epochs)
+
+        print("Train score:", score_train)
+        print("Eval score:", run_eval(config, storage, 50))
+        print(f"MuZero played {config.nb_episodes * (loop + 1)} "
+              f"episodes and trained for {config.nb_epochs * (loop + 1)} epochs.\n")
+
+    return storage.latest_network()
+
+if __name__ == '__main__':
+    gym.make('Abadia-v4')
+    config = make_abadia_config()
+    muzero(config)
